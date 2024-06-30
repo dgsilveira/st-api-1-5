0f71e6688749498257fd54c72106f17a13a0778b
Douglas Silveira <douglasgsilveira@gmail.com>
2024-06-30 08:18:58 -0300

    API simples com ADO.Net

---
+++ st-api/M1.Cadastro.API/PessoaInsert.cs
@@ -0,0 +1,9 @@
+﻿namespace M1.Cadastro.API
+{
+    public class PessoaInsert
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
