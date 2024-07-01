386ddafc4f5af17bd77c85f13a20113bd3277f9b
Douglas Silveira <douglasgsilveira@gmail.com>
2024-06-30 21:08:45 -0300

    API simples com EF

---
+++ st-api/M2.Cadastro.API/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace M2.Cadastro.API
+{
+    internal class Contexto : DbContext
+    {
+        private string connectionString = "Data Source=DELL_DOUG;Initial Catalog=EstudyDB;User ID=sa;Password=sa1234;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(connectionString);
+        }
+    }
+}
