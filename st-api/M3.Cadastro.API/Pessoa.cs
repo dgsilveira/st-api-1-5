bcafbf6d181033805de6e81ca9eaa8c68fdd9de7
Douglas Silveira <douglasgsilveira@gmail.com>
2024-07-02 22:27:29 -0300

    API com divisão de responsabilidades tendo controller, application e repository

---
+++ st-api/M3.Cadastro.API/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿namespace M3.Cadastro.API
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataModificacao { get; set; }
+    }
+}
---
+++ st-api/M3.Cadastro.API/PessoaApplication.cs
@@ -0,0 +1,41 @@
+﻿namespace M3.Cadastro.API
+{
+    public class PessoaApplication
+    {
+        private PessoaRepository pessoaRepository;
+
+        public PessoaApplication()
+        {
+            pessoaRepository = new PessoaRepository();
+        }
+
+        public IEnumerable<Pessoa> ListarPessoas()
+        {
+            var pessoas = pessoaRepository.ListarPessoas();
+
+            return pessoas;
+        }
+
+        public Pessoa BuscarPorId(int id)
+        {
+            var pessoa = pessoaRepository.BuscarPorId(id);
+
+            return pessoa;
+        }
+
+        public void Inserir(PessoaInsert pessoa)
+        {
+            pessoaRepository.Inserir(pessoa);
+        }
+
+        public void Delete (int id)
+        {
+            pessoaRepository.Delete(id);
+        }
+
+        public void Atualizar(int id, PessoaUpdate pessoa)
+        {
+            pessoaRepository.Atualizar(id, pessoa);
+        }
+    }
+}
